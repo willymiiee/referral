e9c4ad2e52ee9fd891c5dba8b21819e54601b3c9
Willy Nugraha <willymiiee@gmail.com>
2018-02-24 02:26:52 +0700

    earning model & migration

---
+++ app/Models/Earning.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Earning extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'member_id',
+        'bonus',
+        'type'
+    ];
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
